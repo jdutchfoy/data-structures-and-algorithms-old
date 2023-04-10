e633add267d2c98661060547a9ad714262fa786a
dutch foy <jdutchfoy@gmail.com>
2023-04-10 13:23:13 -0700

    challenge 3

---
+++ python/code_challenges/array-binary-search/binary_search.py
@@ -0,0 +1,12 @@
+def BinarySearch(sorted_array, search_key):
+
+
+start = 0
+end = lenalist)
+while start < end:
+mid=start + end)//2
+if alist[mid] > key:
+and = Mid
+elif alist[mid] < key:
+start=mid + 1
+else: return mid return -1
